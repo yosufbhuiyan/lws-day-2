ec97bdf56bab81e9a22572866b9887e15300b41c
Yosuf Bhuiyan <yosufbhuiyanbusiness@gmail.com>
2023-01-06 23:43:41 +0600

    first commit

---
+++ style.js
@@ -0,0 +1,18 @@
+document.getElementById("ami").innerHTML ="Hey there";
+
+function myFunction(){
+    document.getElementById("tmi").innerHTML= "Changed"
+}
+document.getElementById('math').innerHTML = 3 + 9;
+
+document.write(5+5);
+
+
+document.getElementById("one").innerHTML = 34 * 7;
+
+window.alert(5+ 12);
+
+alert("youf.17")
+
+console.log( "I am a Developer");
+
